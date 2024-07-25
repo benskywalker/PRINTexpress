c1493e8afebb547fb3a8bb727fb735aeb86ead48
nickaking02@gmail.com <nickaking02@gmail.com>
2024-07-25 16:06:56 -0400

    refactor: Update connections route to fetch sender and receiver names based on document ID

--- routes/index.js
+++ routes/index.js
@@ -305,65 +305,64 @@ router.get('/connections/:id', async (req, res) => {
 }
 );
 //get all documents sent and received by a person
-router.get('/documents/:id', async (req, res) => {
+router.get('/documents', async (req, res) => {
   console.log('GET request received');
-  const personID = req.params.id;
 
   const query = `
     SELECT
-      p.personID AS senderID,
-      CONCAT(p.firstName, ' ', p.lastName) AS sender,
-      p.firstName AS senderFirstName,
-      p.middleName AS senderMiddleName,
-      p.lastName AS senderLastName,
-      p.suffix AS senderSuffix,
-      p.biography AS senderBiography,
-      r.personID AS receiverID,
-      CONCAT(r.firstName, ' ', r.lastName) AS receiver,  
-      r.firstName AS receiverFirstName,
-      r.middleName AS receiverMiddleName,
-      r.lastName AS receiverLastName,
-      r.suffix AS receiverSuffix,
-      r.biography AS receiverBiography,
-      pd.docID AS document,
-      d.importID,
-      d.collection,
-      d.abstract,
-      DATE_FORMAT(d.sortingDate, '%Y-%m-%d') AS date,
-      d.letterDate,
-      d.isJulian,
-      d.researchNotes,
-      d.customCitation,
-      d.docTypeID,
-      d.languageID AS documentLanguageID,
-      d.repositoryID,
-      d.dateAdded,
-      d.status,
-      d.whoCheckedOut,
-      d.volume,
-      d.page,
-      d.folder,
-      d.transcription,
-      d.translation,
-      d.virtual_doc,
-      pdf.pdfURL
-    FROM
-      person p
-    LEFT JOIN person2document pd ON p.personID = pd.personID
-    LEFT JOIN document d ON pd.docID = d.documentID
-    LEFT JOIN person2document pd2 ON pd2.docID = pd.docID
-    LEFT JOIN person r ON pd2.personID = r.personID
-    LEFT JOIN pdf_documents pdf ON d.documentID = pdf.documentID
-    WHERE
-      p.personID != r.personID AND (p.personID = ? OR r.personID = ?)
-    ORDER BY
-      pd.docID`;
+    p.personID AS senderID,
+    CONCAT(p.firstName, ' ', p.lastName) AS sender,
+    p.firstName AS senderFirstName,
+    p.middleName AS senderMiddleName,
+    p.lastName AS senderLastName,
+    p.suffix AS senderSuffix,
+    p.biography AS senderBiography,
+    r.personID AS receiverID,
+    CONCAT(r.firstName, ' ', r.lastName) AS receiver,  
+    r.firstName AS receiverFirstName,
+    r.middleName AS receiverMiddleName,
+    r.lastName AS receiverLastName,
+    r.suffix AS receiverSuffix,
+    r.biography AS receiverBiography,
+    pd.docID AS document,
+    d.importID,
+    d.collection,
+    d.abstract,
+    DATE_FORMAT(d.sortingDate, '%Y/%m/%d') AS date,
+    d.letterDate,
+    d.isJulian,
+    d.researchNotes,
+    d.customCitation,
+    d.docTypeID,
+    d.languageID AS documentLanguageID,
+    d.repositoryID,
+    d.dateAdded,
+    d.status,
+    d.whoCheckedOut,
+    d.volume,
+    d.page,
+    d.folder,
+    d.transcription,
+    d.translation,
+    d.virtual_doc,
+    pdf.pdfURL
+  FROM
+    person p
+  LEFT JOIN person2document pd ON p.personID = pd.personID
+  LEFT JOIN document d ON pd.docID = d.documentID
+  LEFT JOIN person2document pd2 ON pd2.docID = pd.docID
+  LEFT JOIN person r ON pd2.personID = r.personID
+  LEFT JOIN pdf_documents pdf ON d.documentID = pdf.documentID
+  WHERE
+    p.personID != r.personID
+  ORDER BY
+    pd.docID`;
 
   try {
     const db = await dbPromise;
     const promisePool = db.promise();
 
-    promisePool.query(query, [personID, personID]).then(([rows, fields]) => {
+    promisePool.query(query).then(([rows, fields]) => {
       res.json(rows);
     }).catch(error => {
       console.error('Failed to run query:', error);
